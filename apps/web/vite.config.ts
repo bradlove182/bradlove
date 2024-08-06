2f871ef110984747c3e75e1c1c2d70397c5012b5
Bradley Love <brad@bradlove.co.za>
2024-08-06 10:41:16 +0200

    global eslint

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,10 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    test: {
-        include: ["src/**/*.{test,spec}.{js,ts}"],
+    server: {
+        open: true,
     },
+    plugins: [sveltekit()],
     build: {
         commonjsOptions: {
             include: [/@repo\/ui/, /node_modules/],
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
+import { resolve } from "node:path"
+import process from "node:process"
 import antfu from "@antfu/eslint-config"
 
 export default antfu({
     svelte: true,
     typescript: {
-        tsconfigPath: "tsconfig.json",
+        tsconfigPath: resolve(process.cwd(), "tsconfig.json"),
     },
     rules: {
         "no-undef-init": "off",
     },
     stylistic: {
-        indent: 4,
         quotes: "double",
+        indent: 4,
     },
 })
