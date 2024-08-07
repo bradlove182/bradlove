f3b33371b5a8154f368c9dafa8e4c1c9472c5a55
Bradley Love <brad@bradlove.co.za>
2024-08-07 14:16:07 +0200

    stuff

---
+++ apps/consulting/src/lib/navigation/Navigation.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+</script>
+
+<div class="fixed top-0 left-0 w-screen h-fit flex items-center justify-center py-2">
+    <nav class="bg-muted rounded-lg p-4">
+        <ul class="flex gap-2">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </nav>
+</div>
--- apps/consulting/src/routes/+layout.svelte
+++ apps/consulting/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
     import "../app.css"
+    import { ModeWatcher as ThemeWatcher } from "mode-watcher"
+    import Navigation from "$lib/navigation/Navigation.svelte"
 </script>
 
-<slot />
+<ThemeWatcher />
+<Navigation />
+<main class="container">
+    <slot />
+</main>
