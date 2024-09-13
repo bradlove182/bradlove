69a797fe78afba1f56e0b97a2fe97a1baf5c8693
Bradley Love <brad@bradlove.co.za>
2024-09-13 16:19:24 +0200

    module import for tailwind content

--- apps/consulting/tailwind.config.js
+++ apps/consulting/tailwind.config.js
@@ -1,10 +1,11 @@
+import path from "node:path"
 import config from "@repo/ui/tailwind"
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./src/**/*.{html,js,svelte,ts}",
-        "../../packages/ui/**/*.{html,js,svelte,ts}",
+        path.join(path.dirname(require.resolve("@repo/ui")), "**/*.{html,js,svelte,ts}"),
     ],
     presets: [config],
 }
